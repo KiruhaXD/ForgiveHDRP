3a7c1ad182c14727a89d7347116b4e255bf63c01
Kirill Vinokurov <131180897+vvidll@users.noreply.github.com>
2026-07-01 21:07:11 +0300

    fix graphics and add new textures

---
+++ Assets/_Project/Scripts/InteractScripts/SellerInteract.cs
@@ -0,0 +1,38 @@
+using _Project.Scripts.DialogueSystem.DialogueWithSalerScripts;
+using Scripts.TextScripts;
+using TMPro;
+using UnityEngine;
+
+namespace _Project.Scripts.InteractScripts
+{
+    public class SellerInteract : MonoBehaviour, IInteractable
+    {
+        [Header("Dialogue")]
+
+        [SerializeField] DialogueWithSaler dialogue;
+        [SerializeField] TypingText typingText;
+        
+        [SerializeField] TMP_Text tmpTextTalkWithSaler;
+
+        private void Awake()
+        {
+            if (PlayerPrefs.HasKey(CommonDialogueWithSaler.CurrentDialogueIndexKey))
+                DialogueWithSaler.currentDialogueIndex = CommonDialogueWithSaler.LoadCurrentDialogue(DialogueWithSaler.currentDialogueIndex);
+        }
+
+        public void Interact()
+        {
+            // Dialogue system
+            dialogue.StartDialogue();
+
+            Debug.Log("Interact with seller");
+        }
+
+        public string Description() 
+        {
+            tmpTextTalkWithSaler.text = "Talk with seller";
+            return tmpTextTalkWithSaler.text;
+        } 
+    }
+}
+
